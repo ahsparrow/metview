a915de6dfe515b062d17ff9764ee5caefd96c0b1
Alan Sparrow <git@freeflight.org.uk>
2020-06-02 17:42:39 +0100

    Working with Volley

--- app/src/main/java/uk/org/freeflight/metview/MainActivity.kt
+++ app/src/main/java/uk/org/freeflight/metview/MainActivity.kt
@@ -1,12 +1,77 @@
 package uk.org.freeflight.metview
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.ImageRequest
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_refresh -> {
+            refreshMet()
+            true
+        }
+
+        else -> {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun refreshMet() {
+        val tempTextView = findViewById<TextView>(R.id.temperatureTextView)
+        val windTextView = findViewById<TextView>(R.id.windTextView)
+        val gustTextView = findViewById<TextView>(R.id.gustTextView)
+        val minTempTextView = findViewById<TextView>(R.id.minTempTextView)
+        val maxTempTextView = findViewById<TextView>(R.id.maxTempTextView)
+        val maxGustTextView = findViewById<TextView>(R.id.maxGustTextView)
+
+        val queue = RequestQueueSingleton(this)
+        val url = "http://metcloud.freeflight.org.uk/"
+
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+                Response.Listener { response ->
+                    tempTextView.text = String.format("%.1f", response.optDouble("temp", 0.0))
+                    windTextView.text = String.format("%.0f", mph(response.optDouble("wind", 0.0)))
+                    gustTextView.text = String.format("%.0f", mph(response.optDouble("gust", 0.0)))
+                    minTempTextView.text = String.format("%.1f", response.optDouble("min_temp", 0.0))
+                    maxTempTextView.text = String.format("%.1f", response.optDouble("max_temp", 0.0))
+                    maxGustTextView.text = String.format("%.0f", mph(response.optDouble("max_gust", 0.0)))
+                },
+                Response.ErrorListener {
+                }
+        )
+
+        jsonObjectRequest.setShouldCache(false)
+        queue.addToRequestQueue(jsonObjectRequest)
+    }
+
+    private fun mph(val_ms: Double): Double {
+        return val_ms * 2.237
     }
 }
--- app/src/main/java/uk/org/freeflight/metview/MetViewWidget.kt
+++ app/src/main/java/uk/org/freeflight/metview/MetViewWidget.kt
@@ -1,6 +1,5 @@
 package uk.org.freeflight.metview
 
-import android.app.AlarmManager
 import android.app.IntentService
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
@@ -9,59 +8,19 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
-import android.os.SystemClock
 import android.util.Log
+import android.widget.ImageView
 import android.widget.RemoteViews
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.module.AppGlideModule
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.AppWidgetTarget
-import com.bumptech.glide.request.target.Target
-import com.bumptech.glide.request.transition.Transition
+import androidx.core.content.ContextCompat.startActivity
+import com.android.volley.Response
+import com.android.volley.toolbox.ImageRequest
 
 private const val TAG = "METVIEW"
-
-@GlideModule
-class AppGlideModule : AppGlideModule()
 
 /**
  * Implementation of App Widget functionality.
  */
 class MetViewWidget : AppWidgetProvider() {
-    private var alarmMgr: AlarmManager? = null
-    private lateinit var updateIntent: PendingIntent
-
-    override fun onEnabled(context: Context)
-    {
-        Log.v(TAG, "onEnabled")
-        super.onEnabled(context)
-
-        alarmMgr = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        updateIntent = Intent(context, UpdateIntentService::class.java)
-            .let { intent ->
-                PendingIntent.getService(context, 0, intent, 0)
-            }
-
-        alarmMgr?.setInexactRepeating(
-            AlarmManager.ELAPSED_REALTIME,
-            SystemClock.elapsedRealtime(),
-            AlarmManager.INTERVAL_FIFTEEN_MINUTES,
-            updateIntent
-        )
-
-        context.startService(Intent(context, UpdateIntentService::class.java))
-    }
-
-    override fun onDisabled(context: Context) {
-        Log.v(TAG, "onDisabled")
-        super.onDisabled(context)
-
-        alarmMgr?.cancel(updateIntent)
-    }
-
     override fun onUpdate(
         context: Context?,
         appWidgetManager: AppWidgetManager?,
@@ -73,6 +32,7 @@ class MetViewWidget : AppWidgetProvider() {
             .let { intent ->
                 PendingIntent.getService(context, 0, intent, 0)
             }
+        intent.send()
 
         appWidgetIds?.forEach { appWidgetId ->
             val views = RemoteViews(
@@ -87,49 +47,28 @@ class MetViewWidget : AppWidgetProvider() {
         }
     }
 
-    class UpdateIntentService : IntentService("TestIntentService") {
+    class UpdateIntentService : IntentService("WidgetIntentService") {
         override fun onHandleIntent(intent: Intent?) {
             Log.v(TAG, "onHandleIntent")
 
-            val updateViews = buildUpdate(this)
+            val imageUrl = "http://metcloud.freeflight.org.uk/images/summary"
+            val imageRequest = ImageRequest(
+                imageUrl,
+                Response.Listener { response ->
+                    val views = RemoteViews(this.packageName, R.layout.met_view_widget)
+                    views.setImageViewBitmap(R.id.appwidget_image, response)
 
-            val thisWidget = ComponentName(this, MetViewWidget::class.java)
-            val manager = AppWidgetManager.getInstance(this)
-            manager.updateAppWidget(thisWidget, updateViews)
-        }
+                    val thisWidget = ComponentName(this, MetViewWidget::class.java)
+                    val manager = AppWidgetManager.getInstance(this)
+                    manager.updateAppWidget(thisWidget, views)
+                },
+                0, 0,  ImageView.ScaleType.FIT_XY, Bitmap.Config.RGB_565,
+                Response.ErrorListener {}
+            )
+            imageRequest.setShouldCache(false)
 
-        private fun buildUpdate(context: Context) : RemoteViews
-        {
-            val views = RemoteViews(context.packageName, R.layout.met_view_widget)
-            val thisWidget = ComponentName(this, MetViewWidget::class.java)
-
-            val awt: AppWidgetTarget = object: AppWidgetTarget(context, R.id.appwidget_image, views, thisWidget) {
-                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
-                    super.onResourceReady(resource, transition)
-                }
-            }
-
-            Log.v(TAG, "Glide")
-            GlideApp
-                .with(context.applicationContext)
-                .asBitmap()
-                .load("http://metcloud.freeflight.org.uk/images/summary")
-                .listener(object: RequestListener<Bitmap> {
-                    override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Bitmap>?, isFirstResource: Boolean) : Boolean {
-                        Log.e("TAG", "Error loading image")
-                        return false
-                    }
-
-                    override fun onResourceReady(resource: Bitmap?, model: Any?, target: Target<Bitmap>?, dataSource: DataSource?, isFirstResource: Boolean) : Boolean {
-                        return false
-                    }
-                })
-                .diskCacheStrategy(DiskCacheStrategy.NONE)
-                .skipMemoryCache(true)
-                .fitCenter()
-                .into(awt)
-
-            return views
+            val queue = RequestQueueSingleton.getInstance(this).requestQueue
+            queue.add(imageRequest)
         }
     }
 }
